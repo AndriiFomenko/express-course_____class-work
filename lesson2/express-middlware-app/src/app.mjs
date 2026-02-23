25a9cfc5838a9e1992d7dde172fac648c3346101
Alecto <fomenkoandrey1978@gmail.com>
2026-02-23 20:31:53 +0200

    feat: add notFoundHandler middleware for 404 errors

--- lesson2/express-middlware-app/src/app.mjs
+++ lesson2/express-middlware-app/src/app.mjs
@@ -2,6 +2,7 @@ import express from 'express'
 import router from './routes/index.mjs'
 import { errors } from 'celebrate'
 import { logger } from './middlewares/logger.mjs'
+import { notFoundHandler } from './middlewares/notFoundHandler.mjs'
 import { errorHandler } from './middlewares/errorHandler.mjs'
 
 const PORT = 3000
@@ -10,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(logger)
 app.use(router)
+app.use(notFoundHandler)
 app.use(errors())
 app.use(errorHandler)
 
